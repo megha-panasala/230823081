844429f4b98e02f0be48c21b2211048d7876d4a5
megha-pansala <meghapanasala@gmail.com>
2024-03-29 08:29:46 +0530

    add

---
+++ src/Resume.js
@@ -0,0 +1,117 @@
+import React from "react";
+function Resume(){
+    return(
+        <div className="container" style={{}}>
+            <div className="row" style={{'backgroundColor':'pink'}}>
+                
+                <div style={{'textAlign':'center','margin':'5px','padding':'20px','display':'flex'}} >
+                    <img src="logo.png" style={{'height':'120px','width':'80px','marginTop':'-50px','marginLeft':'350px'}}></img>
+                    <h3 style={{'marginLeft':'10px'}}>ISHAAN AGARVAL</h3>
+                </div>
+                <div style={{'backgroundColor':'black','display':'flex'}}>
+                    <p style={{'marginBottom':'0px','color':'white','marginLeft':'400px','paddingTop':'5px' }}>New delhi india-1104 </p>
+                    <ul style={{'color':'white','paddingTop':'5px'}}>
+                        <li>+5418565964</li></ul>
+                    <ul style={{'color':'white','paddingTop':'5px'}}><li>+5418565964</li></ul>
+                    
+               </div>
+            </div>
+            <div className="row">
+                <div className="col-4"style={{"borderRight":"2px solid black"}}>
+                    <div style={{'marginLeft':'-20px'}}>
+                    <h3 style={{'marginTop':'25px','marginLeft':'15px' }}>CORE QUALIFICATIONS</h3>
+                    <ul style={{'marginLeft':'15px'}}>
+                        <li>case regester opration</li>
+                        <li>case regester opration</li>
+                        <li>case regester opration</li>
+                        <li>case regester opration</li>
+                        <li>case regester opration</li>
+                        <li>case regester opration</li>
+                        <li>case regester opration</li>
+                        <li>case regester opration</li>
+                    </ul>                    
+                    </div>
+                    <div style={{'borderBottom':'2px solid black', 'width':'428px'}} ></div>
+                    <div>                   
+                        <h3 style={{'marginTop':'25px'}}>EDUCATION</h3>
+                        <p>Financial acounting 2018 Oxford software institude &Oxford school of english-New delhin IndiaFinanci acounting 2018 Oxford software institude &Oxford english-New delhin India</p>
+                    </div>
+                    <div style={{'borderBottom':'2px solid black', 'width':'428px'}} ></div>
+                    <div>
+                    <h3 style={{'marginTop':'25px'}}>LANGUAGES
+                    </h3>
+                    <ul>
+                        <li>hindu</li>
+                        <li>english</li>
+                        <li>bengal</li>
+                    </ul>
+                    
+                    </div>
+                    <div style={{'borderBottom':'2px solid black', 'width':'428px'}} ></div>
+                    <div>
+                        <h3 style={{'marginTop':'25px'}}>ADDITIONAL INFORMATION</h3>
+                        <ul>
+                            <li>
+                                jnduhouehouwa kjuhfieuwfhie7shyf rekuhrieuyhtirigtuh urht3iey
+                            </li>
+                            <li>
+                                jnduhouehouwa
+                            </li>
+                        </ul>
+                        <div style={{'borderBottom':'2px solid black', 'width':'428px'}} ></div>
+                    </div>
+                    <h3 style={{'marginTop':'25px'}}>INTRESTS</h3>
+                    <ul>
+                        <li>
+                            jnduhouehouwa kjuhfieuwfhie7shyf rekuhrieuyhtirigtuh urht3iey
+                        </li>
+                        <li>
+                            jnduhouehouwa
+                        </li>
+                        <li>
+                            jnduhouehouwa
+                        </li>
+                    </ul>
+                </div>
+                
+                <div className="col-8" style={{'borderLeft':'20px','padding':'0px'}}>
+                {/* <div style={{'borderBottom':'2px solid black', 'width':'428px'}} ></div>    */}
+                <div>
+                    <h3 style={{'marginTop':'25px','paddingLeft':'25px'}}>PROFESSIONAL SUMMARY</h3>
+                    <p style={{'paddingLeft':'25px'}}> dnsejklaf useirh iuetieruhur uhriugiyg rh hrighiarhlgowiyws jkehfiresgiyesgilsek keusrhiuleshgieshgieslugh eskjrheurhgeurhuers krehoerughouerghuhgiuhrhr hti9e4thige4shgiorshws iuhtri4weyghiwehiwehgwiygfiy4egi4uegs wuiygfoygeslisurhwiayhiu  kuhro3y49 </p>
+                    
+                </div>
+                <div style={{'borderBottom':'2px solid black','width':'870px' }} ></div>
+                <h3 style={{'marginTop':'25px','paddingLeft':'25px'}}>EXPERIENCE</h3>
+                <p style={{'paddingLeft':'25px'}}>oifhqiuwhfeihfqeiuh egfuygfqgfygrfy y24wygrfrrrrrrrrrrrrrrr thgi4tg5iw4y wy4hri4gt4g2qguyguygrthgieti4wh uthi4thw4ihgwihg uihye4yti4eygh</p>
+                <ul>
+                    <li>incfreadedjg khiiehrf3ojfra ijfoanjnevljgusbvg ljhrfouehaengwr;gjebdav jgdanj lhterhlejhnqiuqegteiughoqrehgru;qh;gheghwtiughtiuegieugithgitwhgiuirjo45twuh</li>
+                    <li>incfreadedjg khiiehrf3ojfra ijfoanjnevljgusbvg ljhrfouehaengwr;gjebdav jgdanj lhterhlejhnqiuqegteiughoqrehgru;qh;gheghwtiughtiuegieugithgitwhgiuirjo45twuh</li>
+                    <li>incfreadedjg khiiehrf3ojfra ijfoanjnevljgusbvg ljhrfouehaengwr;gjebdav jgdanj lhterhlejhnqiuqegteiughoqrehgru;qh;gheghwtiughtiuegieugithgitwhgiuirjo45twuh</li>
+                </ul>
+                <p style={{'paddingLeft':'2px'}}>
+               
+                    <p style={{'paddingLeft':'25px'}}>kudhiewaygiewya wieuhfriuwhfriwehwf iehfr7ierwyireyhfiueahgwfiuae kjdhfuiergf eskuhfeurihirweyhfiwe iueyriryti7wryqa37 ouwryfarey97ayq</p>
+                    <ul><li style={{'paddingLeft':'2px'}}>ufehwiuerhfirfiyahfi7rgfqi jkwerhiu43whruiw4 ekhruirhy jkdhfiuwehiua weuihfiwehfruiehrfirwhiwhiehiwehsw uhe7iytg7iwsy4</li>
+                    <li style={{'paddingLeft':'2px'}}>ufehwiuerhfirfiyahfi7rgfqi jkwerhiu43whruiw4 ekhruirhyhfiyegriyrh hrirhti7w4htwey4tq4 iuhr7wyt87w4ytw7ytw87yt iuht74wyiyh4i7oh iuh49t4ehgit4ehgith</li>
+                    <li style={{'paddingLeft':'2px'}}>ufehwiuerhfirfiyahfi7rgfqi jkwerhiu43whruiw4 ekhruirhy iuhirhgryhwhwgw uhrtiehaiuhipuhfirefqfqhfief hierhuirerjngrjng rtgtrhugsrnhuiwtehtgwiuhh</li>
+                    <li style={{'paddingLeft':'2px'}}>ufehwiuerhfirfiyahfi7rgfqi jkwerhiu43whruiw4  kerhfiyerh erhgyrhy8rwhg rhewry8weekhruirhy jfgerygfysf ehfuieshiushisehgihgih</li></ul>
+
+                    <p style={{'paddingLeft':'25px'}}>kudhiewaygiewya wieuhfriuwhfriwehwf iehfr7ierwyireyhfiueahgwfiuae kjdhfuiergf eskuhfeurihirweyhfiwe iueyriryti7wryqa37 ouwryfarey97ayq</p>
+                    <ul ><li style={{'paddingLeft':'2px'}}>ufehwiuerhfirfiyahfi7rgfqi jkwerhiu43whruiw4 ekhruirhy jkdhfiuwehiua weuihfiwehfruiehrfirwhiwhiehiwehsw uhe7iytg7iwsy4</li>
+                    <li style={{'paddingLeft':'2px'}}>ufehwiuerhfirfiyahfi7rgfqi jkwerhiu43whruiw4 ekhruirhyhfiyegriyrh hrirhti7w4htwey4tq4 iuhr7wyt87w4ytw7ytw87yt iuht74wyiyh4i7oh iuh49t4ehgit4ehgith</li>
+                    </ul>
+                
+                </p>
+                </div>
+                
+                
+
+            </div>
+            
+        
+         
+        </div>
+    )
+}
+export default Resume;
